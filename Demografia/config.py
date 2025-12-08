17d133a3c289485a67f4cc238626ccbac4817ba9
julia-8467 <julialada8@gmail.com>
2025-12-08 12:46:57 +0100

    Update config.py

--- Demografia/config.py
+++ Demografia/config.py
@@ -4,8 +4,9 @@ APP_ENV = os.getenv('APP_ENV', 'development')
 
 DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'postgres')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'postgres')
-DATABASE_HOST = os.getenv('DATABASE_HOST', 'db')
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
 DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'demografia')
 
 TEST_DATABASE_NAME = os.getenv('TEST_DATABASE_NAME', 'demografia_test')
+
