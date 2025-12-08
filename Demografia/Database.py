ad00845584dd833b9f97ed3563d2b0b4502e71d7
julia-8467 <julialada8@gmail.com>
2025-12-08 12:30:28 +0100

    Update Database.py

--- Demografia/Database.py
+++ Demografia/Database.py
@@ -1,29 +1,32 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from . import config
-from .config import DATABASE_USERNAME
 
-DATABASE_USERNAME= config.DATABASE_USERNAME
-DATABASE_PASSWORD = config.DATABASE_PASSWORD
-DATABASE_HOST = config.DATABASE_HOST
-DATABASE_NAME = config.DATABASE_NAME
+TESTING = os.getenv("TESTING") == "1"
 
-# Składamy connection string
-SQLALCHEMY_DATABASE_URI = (
-    f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
-)
+if TESTING:
+    # Używamy SQLite w pamięci – zero problemów w CI
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+else:
+    DATABASE_USERNAME = config.DATABASE_USERNAME
+    DATABASE_PASSWORD = config.DATABASE_PASSWORD
+    DATABASE_HOST = config.DATABASE_HOST
+    DATABASE_NAME = config.DATABASE_NAME
+    DATABASE_PORT = config.DATABASE_PORT
 
-# Tworzenie silnika SQLAlchemy
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}"
+        f"@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+    )
 
-# Tworzenie sesji
+engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=False)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Baza do deklarowania modeli
 Base = declarative_base()
 
-# Funkcja do pobierania sesji DB (przydatna w FastAPI np.)
+
 def get_db():
     db = SessionLocal()
     try:
